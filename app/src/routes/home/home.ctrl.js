42530f3736192d03b19eea7c82c44a0d124683ae
Leegeonu <lgu_lgu@naver.com>
2022-09-05 15:24:11 +0900

    로그인 fetch 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,3 +1,8 @@
+const users = {
+  id: ["abc", "def", "qwer"],
+  passward: ["123", "456", "123456"],
+}
+
 const output = {
   home: (req, res) => {
     res.render("home/index");
@@ -10,7 +15,21 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    console.log(req.body);
+    const id = req.body.id,
+      passward = req.body.passward;
+      
+    if(users.id.includes(id)){
+      const idx = users.id.indexOf(id);
+      if(users.passward[idx] === passward){
+        return res.json({
+          success: true,
+        })
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하였습니다.",
+    })    
   }
 }
 
