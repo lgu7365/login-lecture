7c473d59ef1bb56d9d2cf5aa1f8210ffbca99946
Leegeonu <lgu_lgu@naver.com>
2022-09-15 10:12:06 +0900

    회원가입 만들기

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,4 +1,5 @@
 const id = document.querySelector('#id');
+const name = document.querySelector('#name');
 const psword = document.querySelector('#psword');
 const confirmPsword = document.querySelector('#confirm-psword');
 const registerbtn = document.querySelector('#button');
@@ -6,8 +7,12 @@ const registerbtn = document.querySelector('#button');
 registerbtn.addEventListener('click', register);
 
 function register(){
+  if(!id.value) return alert('아이디를 입력해주십시오.');
+  if(psword.value !== confirmPsword.value) return alert('비밀번호가 일치하지 않습니다..');
+
   const req = {
     id: id.value,
+    name: name.value,
     password: psword.value,
   }
 
